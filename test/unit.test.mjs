c1bd1abe9be57e5b30ee410cd1ee9a0c0caa020d
Scott McGinness <mcginness.s@gmail.com>
2024-06-07 14:38:55 +0100

    Fill out unit tests

--- test/unit.test.mjs
+++ test/unit.test.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 import babelParser from '@babel/parser';
 import babelTraverseModule from '@babel/traverse';
 import assert from 'node:assert';
@@ -5,37 +6,127 @@ import fs from 'node:fs/promises';
 import path from 'node:path';
 import { afterEach, beforeEach, it } from 'node:test';
 import { strykerPlugins as sut } from '../src/index.mjs';
+import dedent from 'dedent';
+import { EOL } from 'node:os';
 
 /** @type {typeof babelTraverseModule} */
+// @ts-expect-error
 const babelTraverse = babelTraverseModule.default;
 
 const filePath = (name) => path.join(import.meta.dirname, 'fixtures', name);
 
-const file = async (name) => babelParser.parse(await fs.readFile(filePath(name), { encoding: 'utf-8' }));
+const file = async (name) => {
+  /** @type {import('@babel/parser').ParserOptions | undefined} */
+  const opts = name.endsWith('mjs') ? { sourceType: 'module' } : undefined;
 
-const run = async (node) => {
-  return sut[0].value.shouldIgnore(node);
+  const source = await fs.readFile(filePath(name), { encoding: 'utf-8' });
+  return { input: babelParser.parse(source, opts), source };
 };
 
 const EXPECTED_IGNORE_MESSAGE = "Anything inside the 'meta' property of an ESLint rule definition can be ignored.";
+
+const runExpecting = async (fileName, expectedIgnoredPart, canHaveIgnores) => {
+  const { input, source } = await file(fileName);
+  let actualIgnoredPart = '';
+  babelTraverse(input, {
+    enter(path) {
+      const result = sut[0].value.shouldIgnore(path);
+
+      if (result) {
+        actualIgnoredPart = source.slice(path.node.start, path.node.end);
+        if (canHaveIgnores) {
+          assert.equal(result, EXPECTED_IGNORE_MESSAGE);
+          path.stop();
+        } else {
+          throw new Error(`Did not expect to get any ignored part, but got one:${EOL}---${EOL}${actualIgnoredPart}---`);
+        }
+      }
+    },
+  });
+
+  assert.equal(actualIgnoredPart, expectedIgnoredPart);
+};
+
+const runExpectingIgnoredPart = async (fileName, expectedIgnoredPart) =>
+  runExpecting(fileName, expectedIgnoredPart, true);
+
+const runExpectingNoIgnores = async (fileName) => runExpecting(fileName, '', false);
 
 it('exports a Stryker plugin', async () => {
   assert(sut.length === 1);
 });
 
 it('ignores correctly in meta-export', async () => {
-  const input = await file('meta-export.cjs');
+  await runExpectingIgnoredPart(
+    'meta-export.cjs',
+    dedent`
+      exports.meta = {
+        type: 'type',
+        docs: {},
+        schema: [],
+      }`.replace(/\n/g, EOL),
+  );
+});
 
-  let path = null;
+it('does not ignore anything in meta-object-wrong-type-type', async () => {
+  await runExpectingNoIgnores('meta-object-wrong-type-type.mjs');
+});
 
-  babelTraverse(input, {
-    AssignmentExpression(p) {
-      path = p;
-    },
-  });
+it('does not ignore anything in meta-object-wrong-docs-type', async () => {
+  await runExpectingNoIgnores('meta-object-wrong-docs-type.mjs');
+});
 
-  assert.notEqual(path, null);
-  const result = await run(path);
+it('does not ignore anything in meta-object-wrong-schema-type', async () => {
+  await runExpectingNoIgnores('meta-object-wrong-schema-type.mjs');
+});
 
-  assert.equal(result, EXPECTED_IGNORE_MESSAGE);
+it('ignores correctly in meta-object', async () => {
+  await runExpectingIgnoredPart(
+    'meta-object.mjs',
+    dedent`
+      meta: {
+          type: 'type',
+          docs: {},
+          schema: [],
+        }`.replace(/\n/g, EOL),
+  );
+});
+
+it('ignores correctly in meta-object-extra-props', async () => {
+  await runExpectingIgnoredPart(
+    'meta-object-extra-props.mjs',
+    dedent`
+      meta: {
+          type: 'type',
+          docs: {},
+          schema: [],
+          messages: {
+            message1: 'abc',
+          },
+        }`.replace(/\n/g, EOL),
+  );
+});
+
+it('does not ignore anything in meta-object-missing-type', async () => {
+  await runExpectingNoIgnores('meta-object-missing-type.mjs');
+});
+
+it('does not ignore anything in meta-object-missing-docs', async () => {
+  await runExpectingNoIgnores('meta-object-missing-docs.mjs');
+});
+
+it('does not ignore anything in meta-object-missing-schema', async () => {
+  await runExpectingNoIgnores('meta-object-missing-schema.mjs');
+});
+
+it('does not ignore anything in no-meta', async () => {
+  await runExpectingNoIgnores('no-meta.mjs');
+});
+
+it('does not ignore anything in export-same-properties-not-called-meta', async () => {
+  await runExpectingNoIgnores('export-same-properties-not-called-meta.cjs');
+});
+
+it('does not ignore anything in object-same-properties-not-called-meta', async () => {
+  await runExpectingNoIgnores('object-same-properties-not-called-meta.mjs');
 });
